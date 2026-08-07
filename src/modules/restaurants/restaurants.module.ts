c140d1518341d1516145453a746626b19a87d18d
Belén Buzatto <varelabelencita23@gmail.com>
2026-08-07 15:41:30 -0300

    solución a bug de modulo de restaurante

--- src/modules/restaurants/restaurants.module.ts
+++ src/modules/restaurants/restaurants.module.ts
@@ -4,9 +4,12 @@ import { PrismaModule } from '../../core/database/prisma.module';
 
 import { RestaurantController } from './controllers/restaurant.controller';
 import { RestaurantService } from './services/restaurant.service';
-
+import { CloudinaryModule } from '../../core/integrations/cloudinary/cloudinary.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    CloudinaryModule,
+  ],
   controllers: [RestaurantController],
   providers: [RestaurantService],
   exports: [RestaurantService],
